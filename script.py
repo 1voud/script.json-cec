07eb5e66cde18c85bdc8e09daa980eafe0dbf7af
1voud <bartox@outlook.com>
2021-03-20 19:08:10 +0100

    Cleanup

--- script.py
+++ script.py
@@ -1,27 +1,23 @@
 import xbmc
-#import urllib
 import sys
 import time
 
 try:
-        xbmc.log(str(sys.argv[1]), 2)
-#        params = urllib.parse_qs('&'.join(sys.argv[1:]))
-#        xbmc.log(str(params), 2)
-#        command = params.get('command',None)
-#        xbmc.log(str(command), 2)
+    xbmc.log(str(sys.argv[1]), 2)
 except:
-        xbmc.log("Failed", 2)
+    xbmc.log("Failed", 2)
+        
 if sys.argv[1] == 'command=activate':
-        xbmc.executebuiltin('CECActivateSource')
+    xbmc.executebuiltin('CECActivateSource')
 
 elif sys.argv[1] == 'command=toggle':
-        xbmc.executebuiltin('CECToggleState')
+    xbmc.executebuiltin('CECToggleState')
 
 elif sys.argv[1] == 'command=standby':
-        xbmc.executebuiltin('CECStandby')
+    xbmc.executebuiltin('CECStandby')
 
 elif sys.argv[1] == 'command=stop_and_standby':
-        if xbmc.Player().isPlaying():
-                xbmc.executebuiltin("PlayerControl(Stop)")
-                time.sleep(3)
-        xbmc.executebuiltin('CECStandby')
+    if xbmc.Player().isPlaying():
+        xbmc.executebuiltin("PlayerControl(Stop)")
+        time.sleep(3)
+    xbmc.executebuiltin('CECStandby')
